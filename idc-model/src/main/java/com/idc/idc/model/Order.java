ef76e441584bf110404eeb42da51b9f3d0af727d
Ilgizar <ilgizar1313@gmail.com>
2018-10-03 22:22:43 +0300

    OrderService

---
+++ idc-model/src/main/java/com/idc/idc/model/Order.java
@@ -0,0 +1,54 @@
+package com.idc.idc.model;
+
+import com.idc.idc.model.abstracts.AbstractAuditableEntity;
+import com.idc.idc.model.enums.UserRole;
+import com.idc.idc.model.enums.UserState;
+import lombok.*;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Table(name = "orders")
+@Entity
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order extends AbstractAuditableEntity {
+    public static final String DUE_DATE = "due_date";
+    public static final String FROM = "from";
+    public static final String TO = "to";
+    public static final String STATUS = "status";
+    public static final String WEIGHT = "weight";
+    public static final String WORTH = "worth";
+    public static final String DESCRIPTION = "description";
+    public static final String CONTACTS = "contacts";
+
+    @Column(name = DUE_DATE)
+    @Type(type = "java.time.LocalDateTime")
+    private LocalDateTime due_date;
+
+    @Column(name = FROM)
+    private double from;
+
+    @Column(name = TO)
+    private double to;
+
+    @Column(name = STATUS)
+    private int status;
+
+    @Column(name = WEIGHT)
+    private double weight;
+
+    @Column(name = WORTH)
+    private double worth;
+
+    @Column(name = DESCRIPTION)
+    private String description;
+
+    @Column(name = CONTACTS)
+    private String contacts;
+}
--- idc-service/src/main/java/com/idc/idc/service/UserService.java
+++ idc-service/src/main/java/com/idc/idc/service/UserService.java
@@ -1,6 +1,5 @@
 package com.idc.idc.service;
 
-import com.google.inject.ImplementedBy;
 import com.idc.idc.dto.form.UserRegistrationForm;
 import com.idc.idc.model.User;
 
