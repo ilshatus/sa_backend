42211a9db680fa9ffe0a6fa21a80e5842772addc
Ilshat <g.ilshat.98@mail.ru>
2018-10-10 16:40:48 +0300

    modify user models

--- idc-model/src/main/java/com/idc/idc/model/users/Customer.java
+++ idc-model/src/main/java/com/idc/idc/model/users/Customer.java
@@ -1,6 +1,7 @@
 package com.idc.idc.model.users;
 
-import com.idc.idc.model.abstracts.AbstractUserEntity;
+import com.idc.idc.model.abstracts.AbstractAuditableEntity;
+import com.idc.idc.model.abstracts.AbstractEntity;
 import com.idc.idc.model.enums.UserState;
 import lombok.*;
 
@@ -15,7 +16,20 @@ import javax.persistence.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Customer extends AbstractUserEntity {
+public class Customer extends AbstractAuditableEntity {
+    public static final String EMAIL = "email";
+    public static final String PASSWORD_HASH = "password_hash";
+    public static final String NAME = "name";
+
+    @Column(name = EMAIL, length = 100, unique = true)
+    private String email;
+
+    @Column(name = PASSWORD_HASH, length = 512)
+    private String passwordHash;
+
+    @Column(name = NAME, length = 250)
+    private String name;
+
     @Column
     @Enumerated(EnumType.STRING)
     private UserState state;
--- idc-model/src/main/java/com/idc/idc/model/users/Driver.java
+++ idc-model/src/main/java/com/idc/idc/model/users/Driver.java
@@ -1,8 +1,9 @@
 package com.idc.idc.model.users;
 
-import com.idc.idc.model.abstracts.AbstractUserEntity;
+import com.idc.idc.model.abstracts.AbstractAuditableEntity;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Index;
 import javax.persistence.Table;
@@ -16,5 +17,17 @@ import javax.persistence.Table;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Driver extends AbstractUserEntity {
+public class Driver extends AbstractAuditableEntity {
+    public static final String EMAIL = "email";
+    public static final String PASSWORD_HASH = "password_hash";
+    public static final String NAME = "name";
+
+    @Column(name = EMAIL, length = 100, unique = true)
+    private String email;
+
+    @Column(name = PASSWORD_HASH, length = 512)
+    private String passwordHash;
+
+    @Column(name = NAME, length = 250)
+    private String name;
 }
