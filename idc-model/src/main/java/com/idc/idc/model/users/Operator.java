8b335a72318641c2b8d85d9eef73c6d84e2133c2
Ilshat <g.ilshat.98@mail.ru>
2018-10-10 16:40:48 +0300

    modify user models

--- idc-model/src/main/java/com/idc/idc/model/users/Operator.java
+++ idc-model/src/main/java/com/idc/idc/model/users/Operator.java
@@ -1,8 +1,10 @@
 package com.idc.idc.model.users;
 
-import com.idc.idc.model.abstracts.AbstractUserEntity;
+import com.idc.idc.model.abstracts.AbstractAuditableEntity;
+import com.idc.idc.model.abstracts.AbstractEntity;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Index;
 import javax.persistence.Table;
@@ -16,5 +18,17 @@ import javax.persistence.Table;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Operator extends AbstractUserEntity {
+public class Operator extends AbstractAuditableEntity {
+    public static final String EMAIL = "email";
+    public static final String PASSWORD_HASH = "password_hash";
+    public static final String NAME = "name";
+
+    @Column(name = EMAIL, length = 100, unique = true)
+    private String email;
+
+    @Column(name = PASSWORD_HASH, length = 512)
+    private String passwordHash;
+
+    @Column(name = NAME, length = 250)
+    private String name;
 }
