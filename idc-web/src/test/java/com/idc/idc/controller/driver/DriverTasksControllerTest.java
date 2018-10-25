72e59d2bdbd496c2fbf228f31451731180ac8cbe
Ilgizar <ilgizar1313@gmail.com>
2018-10-25 14:23:00 +0300

    Test skeleton

---
+++ idc-web/src/test/java/com/idc/idc/controller/driver/DriverTasksControllerTest.java
@@ -0,0 +1,20 @@
+package com.idc.idc.controller.driver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DriverTasksControllerTest {
+
+    @Test
+    public void getTasksInProgress() {
+    }
+
+    @Test
+    public void activateTask() {
+    }
+
+    @Test
+    public void completeTask() {
+    }
+}
