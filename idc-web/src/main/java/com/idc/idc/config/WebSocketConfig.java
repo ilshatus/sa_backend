8d437c93aa1727de626598be9e1fb650cc16dae8
Ilshat <g.ilshat.98@mail.ru>
2018-11-14 21:15:04 +0300

    add ssl

--- idc-web/src/main/java/com/idc/idc/config/WebSocketConfig.java
+++ idc-web/src/main/java/com/idc/idc/config/WebSocketConfig.java
@@ -6,22 +6,33 @@ import com.idc.idc.settings.WebSocketSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.IOException;
 
 @Configuration
 public class WebSocketConfig {
     private WebSocketSettings webSocketSettings;
+    private ResourceLoader resourceLoader;
 
     @Autowired
-    public WebSocketConfig(WebSocketSettings webSocketSettings) {
+    public WebSocketConfig(WebSocketSettings webSocketSettings,
+                           ResourceLoader resourceLoader) {
         this.webSocketSettings = webSocketSettings;
+        this.resourceLoader = resourceLoader;
     }
 
     @Bean
-    public SocketIOServer socketIOServer() {
+    public SocketIOServer socketIOServer() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:keystore.p12");
         com.corundumstudio.socketio.Configuration config =
                 new com.corundumstudio.socketio.Configuration();
         config.setHostname(webSocketSettings.getHost());
         config.setPort(webSocketSettings.getPort());
+        config.setKeyStorePassword("pizdablya");
+        config.setKeyStoreFormat("PKCS12");
+        config.setKeyStore(resource.getInputStream());
         return new SocketIOServer(config);
     }
 
